35ba99832a7354f875e0b2ba6d7929de49f650b5
Kyle Bryant <kb064318@ohio.edu>
2019-09-06 16:38:21 -0400

    Adding program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Kyle Bryant>
+ *        Date: <September 6, 2019>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
